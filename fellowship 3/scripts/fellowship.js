ea1705266e2e2584861d1da4f6997423a8ac8620
David Cady Jr <davidcadyjr@mac.com>
2017-05-26 16:34:06 -0500

    parts 1 and 2

--- fellowship 3/scripts/fellowship.js
+++ fellowship 3/scripts/fellowship.js
@@ -20,4 +20,80 @@ var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.body;
 
 
+//Part 1
+var makeMiddleEarth = function(){
+
+var middleEarth = document.createElement("section");
+middleEarth.id = "middle-earth";
+
+var theShire = document.createElement("article");
+var shireh1 = document.createElement("h1");
+var shiretext = document.createTextNode(lands[0]);
+shireh1.appendChild(shiretext);
+theShire.appendChild(shireh1);
+middleEarth.appendChild(theShire);
+
+
+var rivendell = document.createElement("article");
+var rivendellh1 = document.createElement("h1");
+var rivendelltext = document.createTextNode(lands[1]);
+rivendellh1.appendChild(rivendelltext);
+rivendell.appendChild(rivendellh1);
+middleEarth.appendChild(rivendell);
+
+
+var mordor = document.createElement("article");
+var mordorh1 = document.createElement("h1");
+var mordortext = document.createTextNode(lands[2]);
+mordorh1.appendChild(mordortext);
+mordor.appendChild(mordorh1);
+middleEarth.appendChild(mordor);
+
+body.appendChild(middleEarth);
+
+};
+
+makeMiddleEarth();
+
+//Part 2
+var makeHobbits = function(){
+
+	var hobbitsList = document.createElement("ul");
+
+	var frodo = document.createElement('li');
+	frodo.class= "hobbit";
+	var frodotext = document.createTextNode(hobbits[0]);
+	frodo.appendChild(frodotext);
+	hobbitsList.appendChild(frodo);
+
+	var sam = document.createElement("li");
+	sam.class = "hobbit";
+	var samtext = document.createTextNode(hobbits[1]);
+	sam.appendChild(samtext);
+	hobbitsList.appendChild(sam);
+
+	var merry = document.createElement("li");
+	merry.class = "hobbit";
+	var merrytext = document.createTextNode(hobbits[2]);
+	merry.appendChild(merrytext);
+	hobbitsList.appendChild(merry);
+
+	var pip = document.createElement("li");
+	pip.class= "hobbit";
+	var piptext = document.createTextNode(hobbits[3]);
+	pip.appendChild(piptext);
+	hobbitsList.appendChild(pip);
+
+	var list = document.getElementsByTagName("article")[0];
+	list.appendChild(hobbitsList);
+}
+
+makeHobbits();
+
+
+
+
+
+
+
 
